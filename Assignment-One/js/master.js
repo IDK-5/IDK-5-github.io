bb4c976331918d5b5abae92a735c71e6a25f3a94
Assassin <assassin@localhost.localdomain>
2020-04-06 20:14:01 +1000

    file path rename

--- Assignment-One/js/master.js
+++
@@ -1,7 +0,0 @@
-// This file is for the main functionality of the website!
-
-h1Test = document.getElementById('h1');
-
-h1Test.addEventListener('click',function(){
-  console.log("Hello, Team!");
-});
